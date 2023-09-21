4b2c030b18f8c5bf1a2b8af475338eb88ce9cca9
nowuri <109161102+nowuri@users.noreply.github.com>
2023-09-22 00:26:05 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+conn = st.experimental_connection('mysql',type='sql')
+
+df = conn.query('SELECT * from mytable;', ttl=600)
+
+for row in df.itertupeles():
+    st.write(f"{row.name} has a :{row.pet}:")
